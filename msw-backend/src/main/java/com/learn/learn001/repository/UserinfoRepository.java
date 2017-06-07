24fe91755a9f8f0530c0e97c2edd448c5cf7dc9c
liujunbin <976500133@qq.com>
2017-06-07 16:06:27 +0800

    完善Repository 功能

--- msw-backend/src/main/java/com/learn/learn001/repository/UserinfoRepository.java
+++ msw-backend/src/main/java/com/learn/learn001/repository/UserinfoRepository.java
@@ -1,12 +1,10 @@
 package com.learn.learn001.repository;
 
+
 import com.learn.learn001.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by liujunbin on 17/6/7.
- */
-public interface UserinfoRepository extends JpaRepository<UserInfo , String> {
+public interface UserinfoRepository extends JpaRepository<UserInfo , Integer> {
 
-    public UserInfo findByUsername(String name);
+    public UserInfo findByUserName(String userName);
 }
