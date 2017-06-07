d72422e5698d9065507b592772aca85e7d89bc0d
liujunbin <976500133@qq.com>
2017-06-07 18:13:37 +0800

    添加后端jsp 和 BCrypt

--- msw-backend/src/main/java/com/learn/learn001/service/UserInfoServiceImpl.java
+++ msw-backend/src/main/java/com/learn/learn001/service/UserInfoServiceImpl.java
@@ -2,11 +2,15 @@ package com.learn.learn001.service;
 
 import com.learn.learn001.entity.UserInfo;
 import com.learn.learn001.repository.UserinfoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by liujunbin on 17/6/7.
  */
+@Service
 public class UserInfoServiceImpl implements UserInfoService {
+
 
 
     private UserinfoRepository userinfoRepository;
@@ -14,10 +18,13 @@ public class UserInfoServiceImpl implements UserInfoService {
     @Override
     public void save(UserInfo userinfo) {
 
+        userinfoRepository.save(userinfo);
+
     }
 
 
     @Override
+    @Transactional(readOnly = true)
     public UserInfo findByUserName(String userName) {
         return userinfoRepository.findByUserName(userName);
     }
